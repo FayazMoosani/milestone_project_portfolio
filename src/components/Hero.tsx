3e9ba65201983131f5bfae3bd276717ab5139aca
FayazMoosani <160345500+FayazMoosani@users.noreply.github.com>
2024-11-12 17:37:14 +0500

    Add files via upload

---
+++ src/components/Hero.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Hero = () => {
+  return (
+<div
+  id='hero'
+  className='min-h-screen bg-no-repeat bg-[url(/profile-pic.png)] bg-cover'
+  style={{ backgroundSize: "35%", backgroundPosition: "left 100px top 100px" }}
+>
+  
+
+      <Navbar />
+      <div className='container grid lg:grid-cols-2 h-[cals(100hv-60px)]'>
+        <div className='hidden lg:block'></div>
+        <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center'>
+          <div>
+            <p data-aos="zoom-in-up">I&apos;m</p>
+            <p data-aos="zoom-in-up">Fayaz</p>
+            <p data-aos="zoom-in-up">Ahmed</p>
+          </div>
+        </div>
+      </div>
+
+      
+      
+    </div>
+  )
+}
+
+export default Hero
